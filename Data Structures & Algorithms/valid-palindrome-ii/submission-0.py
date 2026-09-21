dd0d049cc11b3b13f1995a04c9d3b76c5bd6c6be
Sam <Samuelwang0110@gmail.com>
2026-09-20 20:12:58 -0400

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        """
+        Pre(s) -> s is alphanumeric string s.t. 0 < len(s) < 100,000
+        We need to return bool b s.t. Post(b) -> b iff s' is a palindrome string
+        after removing at most one character from s.
+
+        Strategy;
+        Two pointer approach, check if left and right pointers are the same.
+        If not, have one skip. Check if left skip results in a valid palindrome, and if 
+        right skip results in valid palindrome. If not, return False.
+        """
+
+        def check_palindrome(l: int, r: int) -> bool:
+            print(f"{l}, {r}")
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        l, r = 0, len(s) - 1
+        skip = True
+
+        while l < r:
+            if s[l] != s[r]:
+                if skip:
+                    skip = False
+                    return any((check_palindrome(l + 1, r), check_palindrome(l, r - 1)))
+                return False
+            l += 1
+            r -= 1
+        return True
